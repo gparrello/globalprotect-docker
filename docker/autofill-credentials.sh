5aa275748dcaeb5df94ff9a522347bf788d8da89
Gerardo Parrello <gerardo.parrello@glovoapp.com>
2026-01-21 15:27:52 +0100

    refactor: replace VNC with xdotool for direct X11 input, remove noVNC

--- docker/autofill-credentials.sh
+++ docker/autofill-credentials.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-VNC_HOST="${VNC_HOST:-localhost}"
-VNC_PORT="${VNC_PORT:-8998}"
+export DISPLAY=:99
 STEP_DELAY="${STEP_DELAY:-5}"
 
 if [[ -z "${GP_USERNAME}" ]] || [[ -z "${GP_PASSWORD}" ]] || [[ -z "${GP_TOTP_SECRET}" ]]; then
@@ -13,45 +12,45 @@ MAX_RETRIES=30
 RETRY_INTERVAL=2
 
 for i in $(seq 1 $MAX_RETRIES); do
-    if nc -z "$VNC_HOST" "$VNC_PORT" 2>/dev/null; then
-        echo "VNC server is ready"
+    if xdpyinfo -display "$DISPLAY" >/dev/null 2>&1; then
+        echo "Xvfb server is ready"
         break
     fi
-    echo "Waiting for VNC server... ($i/$MAX_RETRIES)"
+    echo "Waiting for Xvfb server... ($i/$MAX_RETRIES)"
     sleep $RETRY_INTERVAL
 done
 
 sleep $STEP_DELAY
 
 echo "Typing credentials..."
-vncdo -s "${VNC_HOST}::${VNC_PORT}" key ctrl-a
-vncdo -s "${VNC_HOST}::${VNC_PORT}" type "${GP_USERNAME}"
-vncdo -s "${VNC_HOST}::${VNC_PORT}" key enter
+xdotool key ctrl+a
+xdotool type "${GP_USERNAME}"
+xdotool key Return
 
 sleep $STEP_DELAY
 
-vncdo -s "${VNC_HOST}::${VNC_PORT}" key ctrl-a
-vncdo -s "${VNC_HOST}::${VNC_PORT}" type "${GP_PASSWORD}"
-vncdo -s "${VNC_HOST}::${VNC_PORT}" key enter
+xdotool key ctrl+a
+xdotool type "${GP_PASSWORD}"
+xdotool key Return
 
 sleep $STEP_DELAY
 
-vncdo -s "${VNC_HOST}::${VNC_PORT}" key tab
-vncdo -s "${VNC_HOST}::${VNC_PORT}" key tab
-vncdo -s "${VNC_HOST}::${VNC_PORT}" key enter
+xdotool key Tab
+xdotool key Tab
+xdotool key Return
 
 sleep $STEP_DELAY
 
-vncdo -s "${VNC_HOST}::${VNC_PORT}" key tab
-vncdo -s "${VNC_HOST}::${VNC_PORT}" key tab
-vncdo -s "${VNC_HOST}::${VNC_PORT}" key enter
+xdotool key Tab
+xdotool key Tab
+xdotool key Return
 
 sleep $STEP_DELAY
 
 TOTP_CODE=$(oathtool --totp -b "${GP_TOTP_SECRET}")
-vncdo -s "${VNC_HOST}::${VNC_PORT}" key ctrl-a
-vncdo -s "${VNC_HOST}::${VNC_PORT}" type "${TOTP_CODE}"
-vncdo -s "${VNC_HOST}::${VNC_PORT}" key enter
+xdotool key ctrl+a
+xdotool type "${TOTP_CODE}"
+xdotool key Return
 
 sleep $STEP_DELAY
 
